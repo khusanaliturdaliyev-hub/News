eb4b4fb2cc0bc8ad575b69d0c3320aa6f58f5298
Husanali Turdaliyev <Husanali Turdaliyev>
2026-04-16 18:12:48 +0500

    Restore missing STATIC_URL setting

--- core/settings.py
+++ core/settings.py
@@ -106,6 +106,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
+STATIC_URL = 'static/'
+
 if DEBUG:
     STATICFILES_DIRS = [BASE_DIR / 'static']
 else:
