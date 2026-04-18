41a96e8c900ff7d51c2aa10d31d582834291c908
Husanali Turdaliyev <Husanali Turdaliyev>
2026-04-18 12:07:55 +0500

    fix: DEBUG=True qilindi - media fayllar ko'rinishi uchun

--- core/settings.py
+++ core/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-v_5-e4hs4rv8o-f344i@x2@*=kwvmq&i))n8)7nzo@-go@1ray'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
